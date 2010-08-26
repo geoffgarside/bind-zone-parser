f4c4e7d9ad5e6ecb07f6a17d2d66decc27c215c5
Geoff Garside <geoff@geoffgarside.co.uk>
2010-08-26 11:12:35 +0100

    Add origin field to Zone, capture this from ZoneParser if its discovered

--- lib/bind/zone.rb
+++ lib/bind/zone.rb
@@ -1,11 +1,24 @@
 module Bind
   class Zone
     attr_reader :records
+    attr_accessor :origin
+
     def self.load_file(path)
       new(File.read(path))
     end
     def initialize(zone)
-      @records = ZoneParser.parse(zone)
+      @parser = ZoneParser.new
+      @records = @parser.parse(zone)
+      @origin = @parser.origin
+      update_blank_owners
     end
+    protected
+      def update_blank_owners
+        last_owner = nil
+        @records.each do |record|
+          record[:owner] = last_owner if record[:owner] == ""
+          last_owner = record[:owner]
+        end
+      end
   end
 end
--- test/test_zone.rb
+++ test/test_zone.rb
@@ -1,13 +1,31 @@
 require 'helper'
 
 class TestBindZone < Test::Unit::TestCase
-  context ".load_file" do
+  context "Bind::Zone" do
     setup do
       @zone_file = File.expand_path('../examples/example.com.db', __FILE__)
-    end
-    should "return new zone with the records from the file" do
       @zone = Bind::Zone.load_file(@zone_file)
-      assert !@zone.records.empty?
+    end
+    context ".load_file" do
+      should "return new zone with the records from the file" do
+        assert !@zone.records.empty?
+      end
+    end
+    context "#origin" do
+      should "get origin from zone $ORIGIN definition" do
+        assert_equal "example.com.", @zone.origin
+      end
+    end
+    context "#records" do
+      setup do
+        @records = @zone.records
+      end
+      should "not be empty" do
+        assert !@records.empty?
+      end
+      should "change any blank owners to named" do
+        assert @records.all? { |r| r[:owner] != "" }
+      end
     end
   end
 end
